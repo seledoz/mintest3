7854f993ecf1f335a7b475421c6e2f7f21ea9ca2
seledoz <donavonpfeifer@hotmail.com>
2026-09-19 18:57:36 -0600

    Load unified fire-field walkability module

--- pz-bot.js
+++ pz-bot.js
@@ -34,6 +34,7 @@
     "src/modules/red-text-alert.js",
     "src/modules/cave.js",
     "src/modules/cave-waypoint-tolerance-pathing.js",
+    "src/modules/cave-walk-over-fields-global.js",
     "src/modules/cave-forward-loop.js",
     "src/modules/cave-arrow-keys.js",
     "src/modules/cave-waypoint-actions.js",
