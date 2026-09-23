980cfe02e0f2a6f5ff2f10a0f03fa606e56449e6
seledoz <donavonpfeifer@hotmail.com>
2026-09-22 21:45:18 -0600

    Include Spell Timer in Mintest3 bundle

--- minibia-all.js
+++ minibia-all.js
@@ -55,6 +55,7 @@
     "src/modules/remove-legacy-great-fireball.js",
     "src/modules/anti-paralyze-toggle-fix.js",
     "src/modules/player-mana-potion.js",
+    "src/modules/spell-timer.js",
     "src/modules/player-screen-alert.js",
     "src/modules/monster-xray-alarm.js",
     "src/modules/emergency-mana-ring.js",
