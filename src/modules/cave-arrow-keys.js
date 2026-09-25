7cc08c60137832103852d5cabc851ff329d86b23
seledoz <donavonpfeifer@hotmail.com>
2026-09-25 10:26:29 -0600

    Remove Arrow path waypoint tolerance

--- src/modules/cave-arrow-keys.js
+++ src/modules/cave-arrow-keys.js
@@ -186,7 +186,7 @@ window.__minibiaBotBundle.installCaveArrowKeysModule = function installCaveArrow
     const destinationField = !!getDamagingFieldName(destinationTile);
     matrix.set(`${to.x},${to.y}`, { passable: destinationField && isWalkOverFieldsEnabled() ? true : !!matrix.get(`${to.x},${to.y}`)?.passable, field: destinationField });
 
-    const tolerance = destinationField && isWalkOverFieldsEnabled() ? 0 : Math.max(1, Number(bot.cave?.status?.()?.config?.waypointTolerance) || 0);
+    const tolerance = 0;
     const open = [{ ...from, g: 0, f: heuristic(from, to), parent: null }];
     const closed = new Set();
     const key = (p) => `${p.x},${p.y}`;
